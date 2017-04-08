775585af6f44de96e5aff34578287fb5b2cd1beb
Liang xinzhong <johnson.liangxinzhong@gmail.com>
2017-04-08 20:45:46 +0800

    add upvote, downvote, sorty by votes count, about page, delete show edit button, delete description column

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all
+    @topics = Topic.all.sort_by { |a| a.votes.count }.reverse
   end
 
   # GET /topics/1
@@ -28,7 +28,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
         format.json { render :show, status: :created, location: @topic }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TopicsController < ApplicationController
   def update
     respond_to do |format|
       if @topic.update(topic_params)
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully updated.' }
         format.json { render :show, status: :ok, location: @topic }
       else
         format.html { render :edit }
@@ -59,6 +59,23 @@ class TopicsController < ApplicationController
       format.html { redirect_to topics_url, notice: 'Topic was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def upvote
+    @topic = Topic.find(params[:id])
+    @topic.votes.create
+    redirect_to topics_path
+  end
+
+  def downvote
+    @topic = Topic.find(params[:id])
+    if @topic.votes.count != 0
+      @topic.votes.last.destroy
+      redirect_to topics_path
+    else
+      redirect_to topics_path
+    end
+
   end
 
   private
